fac9fe9ace7240416e915fbe96c6c92b6e5a8d0f
DXM <hanpanpan@outlook.com>
2020-01-21 11:15:01 +0800

    维护

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kugcli
+## 本地运行
+```
+npm install
+sudo npm link
+kug --help
+```
+
+## 发布
+```
+npm publish
+```
--- kug/bin/index.js
+++ kug/bin/index.js
@@ -2,30 +2,15 @@
  //这里需要声明文件运行环境
 const program = require('commander');
 program
-    .version(`kug ${require('../package').version}`)
-    .usage('<command> [options]')
+     .version(`kug ${require('../package').version}`)
+     .usage('<command>')
+// program
+//     .option('-d,--default', 'create a project')
 
 program
-    .command('new <app-name>')
-    .description('新建一个项目')
-    .option('-d,--default', 'create a project')
-    .option('-c, --clone', 'Use git clone when fetching remote preset')
+    .command('init')
+    .description('初始化一个google扩展程序')
     .action(function (name, cmd) {
         require('../lib/index.js').run(name, cmd);
     });
-    program
-    .command('add <plugin-name>')
-    .description('添加一个插件')
-    .option('-d,--default', 'create a project')
-    .option('-c, --clone', 'Use git clone when fetching remote preset')
-    .action(function (name, cmd) {
-        console.log("添加插件完成"+argument[0]);
-        
-    });
-program.on('--help', () => {
-    console.log()
-    console.log(`  Run xxx for detailed usage of given command.`)
-    console.log()
-})
-
 program.parse(process.argv);
